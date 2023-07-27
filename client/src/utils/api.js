c4ae4d635a1b5068e1149d66fefec2c819bc153b
vishnu s <vishnusudeesh390@gmail.com>
2023-07-27 21:09:48 +0530

    2nd base url

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,7 +3,7 @@ import dayjs from "dayjs";
 import { toast } from "react-toastify";
 
 export const api = axios.create({
-  baseURL: "https://full-stack-real-estate.vercel.app/api",
+  baseURL: "https://https://real-estate-app-nu-six.vercel.app//api",
 });
 
 export const getAllProperties = async () => {
