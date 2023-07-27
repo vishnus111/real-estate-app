d8c807f808b57960bb92ebe0b1a0e8fc3bc8be12
vishnu s <vishnusudeesh390@gmail.com>
2023-07-27 21:40:11 +0530

    auth commit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,10 +7,10 @@ import { Auth0Provider } from "@auth0/auth0-react";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Auth0Provider
-     domain="dev-03ifqltxbr6nn0hn.us.auth0.com"
-     clientId="RXlGXkr49Ev5MHpvAC6vKkZ4bVn11iwl"
+     domain="realestatedom.jp.auth0.com"
+     clientId="ncOqtJhDkJSID0Fj8iTUqZXLnorufH4T"
      authorizationParams={{
-      redirect_uri: "https://full-stack-real-estate-youtube-lyart.vercel.app"
+      redirect_uri: "https://real-estate-app-9j2l.vercel.app"
      }}
      audience="http://localhost:8000"
      scope="openid profile email"
